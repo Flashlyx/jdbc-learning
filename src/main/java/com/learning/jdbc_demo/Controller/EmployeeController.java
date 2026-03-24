ae154e1787770e5a5076f25906fd6d5f09496e6d
Flashlyx <jayeesh2003@gmail.com>
2026-03-24 23:29:38 +0530

    test commit

--- src/main/java/com/learning/jdbc_demo/Controller/EmployeeController.java
+++ src/main/java/com/learning/jdbc_demo/Controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.learning.jdbc_demo.Controller;
 
+import com.learning.jdbc_demo.DTO.EmployeeDTO;
 import com.learning.jdbc_demo.Entity.Employee;
 import com.learning.jdbc_demo.Service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    private EmployeeService employeeService;
+    private final EmployeeService employeeService;
 
     EmployeeController(EmployeeService employeeService){
         this.employeeService = employeeService;
@@ -16,8 +17,30 @@ public class EmployeeController {
 
     @PostMapping
     @ResponseBody
-    public String insertData(@RequestBody Employee employee){
-        return employeeService.insertIntoDB(employee);
+    public Employee insertData(@RequestBody EmployeeDTO employeeDTO){
+
+        return employeeService.insertIntoDB(employeeDTO);
+    }
+
+    @DeleteMapping
+    @ResponseBody
+    public String deleteByIdFromDB(@RequestBody  EmployeeDTO employeeDTO){
+
+        return employeeService.deleteByIdFromDB(employeeDTO);
+    }
+
+    @PutMapping
+    @ResponseBody
+    public String updateByIdFromDB(@RequestBody EmployeeDTO employeeDTO){
+
+        return employeeService.updateByIdFromDB(employeeDTO);
+    }
+
+    @GetMapping
+    @ResponseBody
+    public Employee fetchEmployee(@RequestBody EmployeeDTO employeeDTO){
+
+        return employeeService.fetchEmployee(employeeDTO);
     }
 
 }
--- src/main/java/com/learning/jdbc_demo/Service/EmployeeService.java
+++ src/main/java/com/learning/jdbc_demo/Service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.learning.jdbc_demo.Service;
 
+import com.learning.jdbc_demo.DTO.EmployeeDTO;
 import com.learning.jdbc_demo.Entity.Employee;
 import com.learning.jdbc_demo.Repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
@@ -7,14 +8,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeService {
 
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
 
-    EmployeeService(EmployeeRepository employeeRepository){
+
+    EmployeeService(EmployeeRepository employeeRepository) {
+
         this.employeeRepository = employeeRepository;
     }
 
-    public String insertIntoDB(Employee employee){
-        employeeRepository.save(employee);
-        return "Employee data inserted !";
+    public Employee insertIntoDB(EmployeeDTO employeeDTO) {
+
+        Employee employee = new Employee();
+        employee.setId(employeeDTO.getId());
+        employee.setName(employeeDTO.getName());
+
+        return employeeRepository.save(employee);
+    }
+
+    public String deleteByIdFromDB(EmployeeDTO employeeDTO) {
+        employeeRepository.deleteById(employeeDTO.getId());
+        return "Employee Deleted by ID";
+    }
+
+    public String updateByIdFromDB(EmployeeDTO employeeDTO) {
+        Employee existingEmployee = employeeRepository.findById(employeeDTO.getId())
+                .orElseThrow();
+
+        existingEmployee.setName(employeeDTO.getName());
+        employeeRepository.save(existingEmployee);
+
+        return "Employee detail has been updated !";
+    }
+
+    public Employee fetchEmployee(EmployeeDTO employeeDTO) {
+
+        Employee existingEmployee = employeeRepository.findById(employeeDTO.getId())
+                .orElseThrow();
+        return existingEmployee;
     }
 }
